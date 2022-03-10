eb756bd447f8078d34040f2c5e896eb4bb7b56f1
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-03-10 07:22:29 -0600

    Add alternate solution for 8ku codewars challenge

--- nDivByXy-03-07-22.js
+++ nDivByXy-03-07-22.js
@@ -1,9 +1,18 @@
 // Create a function that checks if a number n is divisible by two numbers x AND y. 
 // All inputs are positive, non-zero numbers.
+
+
+//first solution
+// function isDivisible(n, x, y) {
+//     if(n % x === 0 && n % y === 0){
+//         return true
+//     }else{
+//       return false
+//     }
+// }
+
+
+//second solution
 function isDivisible(n, x, y) {
-    if(n % x === 0 && n % y === 0){
-        return true
-    }else{
-      return false
-    }
+  return (n % x === 0 && n % y === 0) ? true : false;
 }
