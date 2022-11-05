011a3950ea179463defc2248a30d1db40991928f
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-11-05 09:20:51 -0500

    Add alternate solution along with notes for shortestWord codewars challenge 7KU.

--- shortestWord-7ku.js
+++ shortestWord-7ku.js
@@ -7,3 +7,18 @@ function findShort(s){
     let arr = s.split(" ").sort((a, b) => a.length - b.length);
     return arr[0].length
   }
+
+
+
+
+  const findShort = s => Math.min(...s.split(' ').map(x => x.length));
+
+
+  // //The Math.min() function returns the smallest of the numbers given as input parameters, 
+  // or Infinity if there are no parameters.
+  // //The map() method creates a new array populated with the results of calling a provided function
+  //  on every element in the calling array.
+
+  //In the last code provided, Math.min is used to return the number with the lowest value, from .split
+  //.split is used to turn s into a new array, 
+  //.map is used on the new array - results from the called function on every element in the calling array
