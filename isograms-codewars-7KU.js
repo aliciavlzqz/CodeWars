48cc854a1ae272db186a1e87c8f0c2e5b0853d5a
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-11-07 15:04:02 -0600

    Add alternate solution for 7ku codewars challenge.

--- isograms-codewars-7KU.js
+++ isograms-codewars-7KU.js
@@ -10,4 +10,10 @@
 function isIsogram(str){
     return !str.match(/([a-z]).*\1/i);
   }
+
+
+  //alternate solution
+  function isIsogram(str){
+    return new Set(str.toUpperCase()).size == str.length;
+  }
   
