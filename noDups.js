de80d21e50d3a82d667e9881bb021691ad4b80db
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-11-10 06:45:50 -0600

    Add instructions for 8ku codewars challenge No Duplicates.

---
+++ noDups.js
@@ -0,0 +1,11 @@
+// Define a function that removes duplicates from an array of numbers and returns
+//  it as a result.
+
+// The order of the sequence has to stay the same.
+//Parameters- array of integers with duplicates
+//Return- array of integers w/ duplicate integers removed
+//Example-assert.deepEqual(distinct([1]), [1]);
+    // assert.deepEqual(distinct([1,2]), [1,2]);
+    // assert.deepEqual(distinct([1,1,2]), [1,2]);
+//Pseudocode- I need to look at each integer in the array, if there is a 
+//duplicate integer, I need to remove it and return the array without changing its order.
