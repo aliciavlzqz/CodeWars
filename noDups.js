0510deb115f5909bb663f6655a6633e478809544
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-11-10 06:47:56 -0600

    Add solution for No Dups codewars challenge.

--- noDups.js
+++ noDups.js
@@ -9,3 +9,14 @@
     // assert.deepEqual(distinct([1,1,2]), [1,2]);
 //Pseudocode- I need to look at each integer in the array, if there is a 
 //duplicate integer, I need to remove it and return the array without changing its order.
+
+
+function distinct(a) {
+    let noDups = []
+    for(let i = 0; i< a.length; i++ ){
+        if(noDups.indexOf(a[i] === -1)){
+            noDups.push(a[i])
+        }
+    }
+        return noDups
+    }
