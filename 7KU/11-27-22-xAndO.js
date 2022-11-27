da3ae9d15c245ece9c66eef48ad7239f140e131f
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-11-27 11:38:34 -0600

    Add new codewars challenge and solution 11-27-22

---
+++ 7KU/11-27-22-xAndO.js
@@ -0,0 +1,17 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must
+//  return a boolean and be case insensitive. The string can contain any char.
+
+
+function XO(str) {
+    var x = 0, // numbers are better
+        o = 0;
+
+    for (var i = 0; i < str.length; i++) { // changed from '<=' to '<'
+        if (str[i].toLowerCase() === "x") {
+            x++;
+        } else if (str[i].toLowerCase() === "o") {
+            o++;
+        }
+    }
+    return x === o;
+}
