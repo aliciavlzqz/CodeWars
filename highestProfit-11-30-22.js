4e566808f1cc820809cc101c92045908c95e62d6
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-11-30 11:25:01 -0600

    Add 7KU code challenge and solution 11-30-22

---
+++ highestProfit-11-30-22.js
@@ -0,0 +1,11 @@
+// Write a function that returns both the minimum and maximum number of the given list/array.
+//[1,2,3,4,5] --> [1,5]
+// [2334454,5] --> [5,2334454]
+// [1]         --> [1,1]
+
+//Math.min returns smallest integer and Mathd.max returns largest integer. 
+//
+function minMax(arr){
+    return[ Math.min(...arr),Math.max(...arr)]
+  ; // fix me!
+  }
