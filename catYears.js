351cae934f0fdfa58a9133f96773e0649b567d1b
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2023-01-01 13:59:54 -0600

    set up new Kata 8ku for codewars

---
+++ catYears.js
@@ -0,0 +1,50 @@
+// Kata Task
+// I have a cat and a dog.
+
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+// humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+
+
+    //**THE FOLLOWING ARE TEST CASES */
+// const Test = require('@codewars/test-compat');
+
+// describe("Example Tests", function() {
+
+//   it("one", function() {
+//     Test.assertSimilar(humanYearsCatYearsDogYears(1), [1,15,15]);
+//   });//Here it passes in the number 1, then passes in 3 values.
+
+//   it("two", function() {
+//     Test.assertSimilar(humanYearsCatYearsDogYears(2), [2,24,24]);
+//   });
+
+//   it("ten", function() {
+//     Test.assertSimilar(humanYearsCatYearsDogYears(10), [10,56,64]);
+//   });
+
+// });
+var humanYearsCatYearsDogYears = function(humanYears) {
+    // Your code here!
+
+    return [humanYears,(15 + ), (15 + )];
+  }
+  
+
+//Parameters-
+//Return-
+//Example-
+//Pseudocode-
