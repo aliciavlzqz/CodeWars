771f9a89d82e876670ae5d693d73411dec52b018
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2023-01-18 06:39:53 -0600

    Add solution for 8ku Kata codewars challenge

--- countPositiveSumNeg.js
+++ countPositiveSumNeg.js
@@ -9,3 +9,44 @@
 // Example
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], 
 // you should return [10, -65].
+
+//Parameters- an array with integers, positive and nevative, as well as empty  
+//Return-an array the sum of all positivie integers in the first element, 
+//the second element s the sum of negative integers
+//Example-   let testData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+//     let actual = countPositivesSumNegatives(testData);
+//     let expected = [10, -65];
+//     assert.deepEqual(actual, expected);
+//   })
+    
+//   it("Testing for fixed test 2", () => {
+//     let testData = [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14];
+//     let actual = countPositivesSumNegatives(testData);
+//     let expected = [8, -50];    
+//     assert.deepEqual(actual, expected);
+//   });
+// });
+//Pseudocode-
+//I need to check if the intger is positive or negative. 
+//then I need to iterate positive nums and add them, add neg nums
+//return my result without forgetting about empty array
+
+function countPositivesSumNegatives(arr) {
+    if (arr === null || arr.length === 0) {
+      return [];
+    }
+  
+    let countPositives = 0;
+    let sumNegatives = 0;
+  
+    for (let num of arr) {
+      if (num > 0) {
+        countPositives++;
+      } else if (num < 0) {
+        sumNegatives += num;
+      }
+    }
+  
+    return [countPositives, sumNegatives];
+  }
+  
